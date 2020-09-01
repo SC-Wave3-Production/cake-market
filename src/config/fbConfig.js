0d70abf2e88ae021f2d4f10f4b519d515a510e1b
Silenthunt <destroyer.rus@gmail.com>
2020-09-01 15:09:14 +0300

    typo in fbConfig, sry

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
 	apiKey: 'AIzaSyBrKaNxMv2PlBwVnwKkRyl9V9pTfcDlDDE',
 	authDomain: 'cakes-7a70e.firebaseapp.com',
 	databaseURL: 'https://cakes-7a70e.firebaseio.com',
-	projectId: 'cakes-7a70e1',
+	projectId: 'cakes-7a70e',
 	storageBucket: 'cakes-7a70e.appspot.com',
 	messagingSenderId: '519557679033',
 	appId: '1:519557679033:web:6e21c7f7a6d379a388d868',
