76a20c50cfb94e63ab2bf025a5ebc2ada01e9378
silencepriest <11>
2020-09-29 01:31:08 +0300

    styles changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import Logo from '../img/logo.png';
 
 export default function Header() {
 	return (
-		<div className="py-12 flex items-center bg-red-200">
+		<div className="py-12 flex items-center md:flex-row flex-col flex-wrap bg-red-200">
 			<img className="m-auto" alt="logo" src={Logo} />
-			<a className="mr-8 rounded-full bg-red-600 p-4" href="http://google.com">
+			<a className="mr-8 rounded-full bg-red-600 p-4 md:mt-0 mt-2" href="http://google.com">
 				заказ по тел 012 345 67 89
 			</a>
 		</div>
